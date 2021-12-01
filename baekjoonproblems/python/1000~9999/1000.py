01b98959da857890ed0424abf8ca8a9e2e8faf05
tlgns1212 <tlgns1212@naver.com>
2021-12-01 16:51:47 +0900

    1000 time minimized

--- baekjoonproblems/python/1000~9999/1000.py
+++ baekjoonproblems/python/1000~9999/1000.py
@@ -1,2 +1,5 @@
-a,b = map(int,input().split())
-print(a+b)
+# a,b = map(int,input().split())
+# print(a+b)
+
+# 다른 사람들이 한 거 보면 아래가 시간 64ms로 시간 최소화인데, 내가 해본 결과 위에께 68ms고 아래가 72ms로 내장함수 안쓰는게 더 좋음
+print(sum(map(int, input().split())))
